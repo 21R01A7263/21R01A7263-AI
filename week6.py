8d2af7b600f0a5bbf518066aef774d1553049b8c
Gaurav Sai Teja <128503039+21R01A7263@users.noreply.github.com>
2023-10-07 10:52:03 +0530

    Update week6.py

--- week6.py
+++ week6.py
@@ -1,6 +1,7 @@
 def is_valid(state):
-    m1,c1,b,m2,c2=state
-    return 0<=m1<=3 and 0<=c1<=3 and 0<=m2<=3 and 0<=c2<=3 and ((m1==0 or m1>=c1) and (m2==0 or m2>=c2))
+    m1,c1,b,m2,c2 = state
+    return 0<=m1<=3 and 0<=c1<=3 and 0<=m2<=3 and 0<=c2<=3 and ((m1==0 or m1>=c1) and (m2==0  or m2>=c2))
+
 def generate_next_states(state):
     m1,c1,b,m2,c2=state
     moves=[(1,0),(2,0),(0,1),(0,2),(1,1)]
@@ -19,34 +20,20 @@ def solve_bfs(start_state):
         current_state,path=queue.pop(0)
         if current_state==(0,0,0,3,3):
             return path
-    for next_state in generate_next_states(current_state):
-        if next_state not in path:
-            queue.append((next_state,path+[next_state]))
+        for next_state in generate_next_states(current_state):
+            if next_state not in path:
+                queue.append((next_state,path+[next_state]))
     return None
 start_state=(3,3,1,0,0)
 solution=solve_bfs(start_state)
 if solution:
+
     a=0
     print("solution path:")
     for state in solution:
         m1,c1,b,m2,c2=state
-    print(f"{m1}missionaries,{c1}cannibols,{'boat on left'if b==1 else 'boat on right'},{m2}missionaries,{c2}cannibals")
-    a=a+1
-    print("total cost is:",a)
+        print(f"{m1} missionaries,{c1} cannibals,{'boat on left' if b==1 else 'boat on right'},{m2} missionaries,{c2} cannibals")
+        a=a+1
+    print("total cost is",a)
 else:
-    print("No Solution found")
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
+    print("No solution found")
